0ef4c93e9ee6d0dfe13a7471c05781795666f2d8
dyf <david.feng@alleninstitute.org>
2022-09-01 20:42:34 -0700

    adding bootstrap so that styles load appropriately

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from "@rjsf/core";
+
 
 const schema = {
   title: "Test form",
@@ -20,23 +22,7 @@ const schema = {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
       <Form schema={schema} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
