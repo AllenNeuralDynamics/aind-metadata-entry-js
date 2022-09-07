a4677696d8e2a23f6275b555d466ab9beb6c6b5d
Mekhla Kapoor <mekhla.kapoor@alleninstitute.org>
2022-09-07 14:20:51 -0700

    feat: rename app and cleanup

--- metadata-entry-app/src/Homepage.js
+++ metadata-entry-app/src/Homepage.js
@@ -1,4 +1,3 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom'; 
 
--- metadata-entry-app/src/renderForm.js
+++ metadata-entry-app/src/renderForm.js
@@ -1,4 +1,3 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from "@rjsf/core";
 import Homepage from './Homepage.js';
