f521d097391eb2c983cd5a3309c3deb52dd78306
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-09-22 16:40:46 -0700

    feat: max width

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-//import DropdownList from './components/DropdownList';
+//import App from './components/App';
+import DropdownList from './components/DropdownList';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DropdownList />
   </React.StrictMode>
 );
 
