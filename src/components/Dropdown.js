0d53e832784790e1054ebdf5d723623b1471f84e
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-10-05 00:13:32 -0700

    feat: cleanup files

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-//import schemas from "./App";
-
-// TODO: list of options. map filepaths (value) to labels; already have this? what's the pt of orange radish ex
-// or, instead of list just put straight in
 
 export default function Dropdown(props) {
+  /*
+  Application to render a dropdown menu for schema-selection.
+    Maps dropdown options to value (string) that corresponds with keys in schema_map.
+    Sets value in state to selected schema. 
+  */
 
   const [value, setValue] = useState('');
 
@@ -19,50 +20,8 @@ export default function Dropdown(props) {
         <option value='data description'>Data Description Schema</option>
         <option value='procedures'>Procedures Schema</option>
         <option value='subject'>Subject Schema</option>
-        <option value='../schemas/processing-schema.json'>Processing Schema</option>
+        <option value='processing'>Processing Schema</option>
       </select>
     </div>
   );
-    /*
-    Application to display a dropdown menu of schemas.
-        Creates schema objects from filepaths
-        Maps schemas to corresponding labels (string) in options (list)
-        Renders dropdown menu with options
-     */
-    
-    /*    const data_description = require('../schemas/data-description-schema.json');
-    data_description.title = "data_description";
-    const procedures = require('../schemas/procedures-schema.json');
-    procedures.title = "procedures";
-    const subject = require('../schemas/subject-schema.json');
-    subject.title = "subject";
-    const processing = require('../schemas/processing.json');
-    processing.title = "processing";
-    */
-
-    /*const options = [
-        {label: 'Data Description Schema', value: '../schemas/data-description-schema.json'},
-        {label: 'Procedures Schema', value:'../schemas/procedures-schema.json' }, 
-        {label: 'Subject Schema', value: '../schemas/subject-schema.json'},
-        {label: 'Processing Schema', value: '../schemas/processing.json'}
-    ]
-
-    const handleData = (event) => {
-        props.handleData(event.target.value)
-    }
-
-    const value = ''
-    return (
-        <div>
-            <h1> AIND Metadata Entry </h1>
-            <label>
-                Select a Schema
-                <select value={value} onChange={handleData}>
-                   {options.map((option) => (
-                    <option value={option.value}>{option.label}</option>
-                   ))}
-                </select>
-            </label>
-        </div>
-    );*/
 };
