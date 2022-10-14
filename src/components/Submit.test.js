fbaf26d2d02e3168d0fb6bf62fe8f7b0e7912c20
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-10-13 21:10:17 -0700

    bug: submit unit test

--- src/components/Submit.test.js
+++ src/components/Submit.test.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import RenderForm from './RenderForm';
-import schema_map from '../utilities/constants';
-
-const sample = schema_map['sample']
 
 it("submits", () => {
-  const { getByText } = render(<RenderForm schema={sample} />);
-  fireEvent.click(getByText("Add"));
-  expect(onSubmit).toHaveBeenCalled();
+    const onSubmit = jest.fn();
+    const { getByLabelText, getByText } = render(<RenderForm schema={'sample'} />);
+    fireEvent.change(getByLabelText(/Full Name/i), { target: { name: 'name' } });
+    fireEvent.click(getByText("Submit"));
+    expect(onSubmit).toHaveBeenCalled();
 });
