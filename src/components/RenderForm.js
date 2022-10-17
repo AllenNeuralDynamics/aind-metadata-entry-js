e83c1baa73955375e68209beba62dc5de964d852
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2022-10-17 12:06:20 -0700

    Bug 26 submit (#35)
    * bug: uses hyperjump as validator (doesnt solve submit issue)
    
    * bug: removed hyperjump and ajv6 packages
    
    * bug: starts submit unit test
    
    * bug: removes  field
    
    * bug: submit fixed; test throws error and fails
    
    * bug: submit unit test
    
    * bug: submit test, missing req fields
    
    * fix: patches a rjsf bug and adds unit tests
    
    * fix: removes redundant div
    
    * fix(setupTests): adds new line
    
    * feat: fixes indent
    
    Co-authored-by: Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>

--- src/components/RenderForm.js
+++ src/components/RenderForm.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Form from '@rjsf/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import schema_map from '../utilities/constants';
+import { checkDraft2020, preProcessing } from '../utilities/schemaHandlers';
+
+const Ajv2020 = require("ajv/dist/2020")
+
+const ajv = new Ajv2020()
 
 export default function RenderForm (props) {
   /*
@@ -11,46 +16,12 @@ export default function RenderForm (props) {
   Returns: 
     Form object 
   */
-  let current_schema = props.schema;
 
-  if (current_schema === '') {
-    return <div> Please select a schema from the dropdown above. </div>
-  };
+  const schemaKey = props.schema;
 
-  const preProcessingHelper = (obj) => {
-    /* 
-    Iterates through schema to make const fields non-fillable
-      Grays out const fields (prop.readOnly) and autofills the field with the const value (prop.default)
-    */ 
-    Object.keys(obj).forEach(key => {
-
-      const prop = obj[key]
-      if (prop.const !== undefined) {
-        prop.readOnly = true;
-        prop.default = prop.const
-      }
-
-      if (typeof(prop) === 'object') {
-        preProcessingHelper(prop)
-      }
-    })
-  }
-
-  const preProcessing = (key) => {
-    /*
-    PreProcessing for schema validation
-      Adds id (same as $schema) field to address ajv5 validation and jsonschema 2020-12 compatibility
-      Uses helper function to make const fields non-fillable
-    */
-    const schema = schema_map[key]
-    if (schema.$schema !== undefined) {
-      schema.id = schema.$schema;
-    }
-
-    preProcessingHelper(schema)
-    
-    return schema;
-  }; 
+  const rawSchema = (schemaKey in schema_map) ? schema_map[schemaKey] : undefined;
+  const validator2020 = (rawSchema && checkDraft2020(rawSchema)) ? ajv : undefined;
+  const processedSchema = rawSchema ? preProcessing(rawSchema) : undefined;
 
   const saveFile = (event) => {
     /* 
@@ -64,15 +35,15 @@ export default function RenderForm (props) {
     const filename = JSON.stringify(props.schema);
     FileSaver.saveAs(blob, `${filename}.json`);
   };
-  current_schema = preProcessing(current_schema);
 
-  return (
-    <div>
-      <Form schema={current_schema}
+  if(processedSchema){
+    return (
+      <Form schema={processedSchema}
+      validator={validator2020} noHtml5Validate
       onSubmit={saveFile} >
       </Form>
-    </div>
   );
-};
-
-
+ } else {
+  return(<div> Please select a schema from the dropdown above. </div>)
+ }
+}
