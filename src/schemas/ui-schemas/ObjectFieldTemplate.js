40f39c403672146683e30d1f7e248c09dbf4b875
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-10-31 11:35:34 -0700

    Feat 28 coordinates (#36)
    * feat: skeleton code using ArrayFieldTemplate
    
    * feat: field-array in index.css; figure out how to make speci
    fic to coordinate arrays
    
    * feat: ui schemas
    
    * feat: 3 ideas for inlining (arrayfieldtemplate,index.css,layout)
    
    * feat: ObjectFieldTemplate in uiSchema
    
    * feat: inline dir and value for all coords entries
    
    * feat: todos and cleanup
    
    * feat: col col-lg-2 css
    
    * feat: adds uiSchema field to <Form> if ephyssession
    
    * feat: removes field-string from index.css
    
    * feat: extra instruction to avoid entering more than one of each coord
    
    * feat: newlines
    
    * Update ObjectFieldTemplate.js

---
+++ src/schemas/ui-schemas/ObjectFieldTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+  const ObjectFieldTemplate = ({ properties, description }) => {
+    return (
+      <div>
+        <div className="row">
+          {properties.map(prop => (
+            <div
+              className="col col-lg-2"
+              key={prop.content.key}>
+            {prop.content}
+            </div>
+          ))}
+        </div>
+        {description}
+      </div>
+    );
+  }
+
+export default ObjectFieldTemplate;
