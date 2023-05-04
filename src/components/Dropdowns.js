869a7b606e4c6e951f11e8b05265900d1a938331
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-05-04 16:20:06 -0700

    Feat 71 versions (#73)
    * 2 dropdowns, version selection works
    
    * rehydrate updates schema version
    
    * ui small fix

---
+++ src/components/Dropdowns.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Dropdowns (props) {
+    /**
+     * Application to render a dropdown menu for schema-selection.
+     * Based on selected schema, renders a dropdown menu for version-selection. 
+     */
+  const [selectedSchemaType, setSelectedSchemaType] = useState('');
+  const [selectedSchemaVersion, setSelectedSchemaVersion] = useState('');
+  
+  const schemaList = props.schemaList
+
+  const schemaTypes = Array.from(
+    new Set(schemaList.map((schema) => schema.split('/')[1]))
+  );
+  schemaTypes.shift()
+
+  const versions = schemaList
+    .filter((schema) => schema.includes(selectedSchemaType))
+    .map((schema) => schema.split('/')[2])
+    .filter((version) => version !== undefined);
+
+  const handleTypeChange = (event) => {
+    props.ParentTypeCallback(event.target.value);
+    setSelectedSchemaType(event.target.value);
+  };
+
+  const handleVersionChange = (event) => {
+    props.ParentVersionCallback(event.target.value);
+    setSelectedSchemaVersion(event.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        id="schema-type-select"
+        value={selectedSchemaType}
+        onChange={handleTypeChange}
+      >
+        <option value="">Select a Schema</option>
+        {schemaTypes.map((schemaType) => (
+          <option key={schemaType} value={schemaType}>
+            {schemaType}
+          </option>
+        ))}
+      </select>
+      <br />
+      <div></div>
+      {selectedSchemaType && (
+        <>
+          <select
+            id="schema-version-select"
+            value={selectedSchemaVersion}
+            onChange={handleVersionChange}
+          >
+            <option value="">Select Version</option>
+            {versions.map((version) => (
+              <option key={version} value={version}>
+                {version}
+              </option>
+            ))}
+          </select>
+        </>
+      )}
+    </div>
+  );
+};
+  
