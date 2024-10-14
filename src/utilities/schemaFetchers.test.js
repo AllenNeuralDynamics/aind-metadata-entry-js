b9a70cdc7bd880234a3fb73f1775bc6c079b14c9
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2024-10-14 15:47:48 -0700

    linter

--- src/utilities/schemaFetchers.test.js
+++ src/utilities/schemaFetchers.test.js
@@ -8,8 +8,8 @@ const SAMPLE_VALID_SCHEMA_LINKS = [
 const SAMPLE_UNFILTERED_SCHEMA_LINKS = [
   'schemas/test_type_1/1.0.0/test_type_1_schema.json',
   'schemas/test_type_1/1.0.1/test_type_1_schema.json',
-  'schemas/test_type_1/2.0.0/test_type_1_schema.json',
-];
+  'schemas/test_type_1/2.0.0/test_type_1_schema.json'
+]
 const EXPECTED_PARSED_SCHEMAS = [
   { type: 'test_type_1', version: '1.0.0', path: 'schemas/test_type_1/1.0.0/test_type_1_schema.json' },
   { type: 'test_type_1', version: '1.0.1', path: 'schemas/test_type_1/1.0.1/test_type_1_schema.json' },
@@ -53,7 +53,6 @@ const SAMPLE_INVALID_FORM_DATAS = [
   {}
 ]
 
-
 describe('parseAndFilterSchemas', () => {
   const originalEnv = process.env
 
@@ -79,19 +78,19 @@ describe('parseAndFilterSchemas', () => {
   })
 
   it('should filter out schemas based on version in Config.REACT_APP_FILTER_VERSIONS', () => {
-    process.env.REACT_APP_FILTER_SCHEMAS = JSON.stringify([]);
+    process.env.REACT_APP_FILTER_SCHEMAS = JSON.stringify([])
     global.Config = {
       REACT_APP_FILTER_VERSIONS: {
-        test_type_1: ['1.0.0', '1.0.1'],
+        test_type_1: ['1.0.0', '1.0.1']
       }
-    };
+    }
 
-    const result = parseAndFilterSchemas(SAMPLE_UNFILTERED_SCHEMA_LINKS);
+    const result = parseAndFilterSchemas(SAMPLE_UNFILTERED_SCHEMA_LINKS)
 
     expect(result).toEqual([
       { type: 'test_type_1', version: '2.0.0', path: 'schemas/test_type_1/2.0.0/test_type_1_schema.json' }
-    ]);
-  });
+    ])
+  })
 })
 
 describe('findSchemaFromFormData', () => {
